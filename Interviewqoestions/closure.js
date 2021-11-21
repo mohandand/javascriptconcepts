fb197832d3ef8a7842dc3f3c9d56cfa38aa21692
mohandand <mohandand@gmail.com>
2021-11-21 12:04:11 -0800

    updated Code

--- Interviewqoestions/closure.js
+++ Interviewqoestions/closure.js
@@ -1,13 +1,14 @@
 /* In the above program we didn't pass the x to function logx. But still we will get  
 x value as hundred if we call the logx function .So that happens because of Closure. */
 
-var x=100;
+//let x=100;
 
 function logx(){
+    let x=100;
     function inner(){
-        x++;
+        //x++;
         console.log(x);
-        var x=200;
+        //var x=200;
 
     }
    inner();

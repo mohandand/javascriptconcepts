1134f56caf73724957464c253998b08fb79d6b17
mohandand <mohandand@gmail.com>
2021-11-30 12:59:56 -0800

    updated code

--- prototype.js
+++ prototype.js
@@ -1,6 +1,6 @@
 //Source : http://sporto.github.io/blog/2013/02/22/a-plain-english-guide-to-javascript-prototypes/
 
-// let's create an alien object to
+// let's create an alien object to see
 var alien = {
     kind: 'alien'
   }

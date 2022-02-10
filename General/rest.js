21677b70f5f3a53f846074b624500c70fa9618a8
mohandand <mohandand@gmail.com>
2022-02-10 15:56:00 -0800

    Rest Operator

--- General/rest.js
+++ General/rest.js
@@ -1,4 +1,5 @@
 function addnums(...args){
+    console.log(args)
     let sum=0;
     for(let num of args){
         sum =sum +num;
@@ -7,3 +8,21 @@ function addnums(...args){
 }
 
 console.log(addnums(1,2,3));
+
+// In above the function can take any number of arguments using rest operator
+// it convert single values into a array
+
+/* if you want to get product of first values and sum 
+of rest all values then we can write below code */
+
+function addmul(a,b,...args){
+    let mul = a*b;
+    let sum=0;
+    for(let num of args){
+        sum =sum +num;
+    }
+    return [mul,sum]
+
+}
+
+console.log(addmul(2,2,1,1,1));

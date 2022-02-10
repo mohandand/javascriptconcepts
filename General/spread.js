ad82d218be7fdeb16470c535f75534be3125f4d5
mohandand <mohandand@gmail.com>
2022-02-10 15:44:33 -0800

    Spread Operator Explanation

--- General/spread.js
+++ General/spread.js
@@ -3,3 +3,6 @@ function add(a,b){
 }
 
 console.log(add(...[2,3]));
+
+//if we send args as array it wwill fail but if we use spread operator
+//Basically it will spread array elments as individual arguments.
